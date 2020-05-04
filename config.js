52a6de25ed2f9790e8bf33ad2c16fd07576bef08
Madhankumar <madhankumar028@gmail.com>
2020-05-04 08:54:50 +0700

    Feature/jam stack (#22)
    * initial commit
    
    * clens up the assets

---
+++ config.js
@@ -0,0 +1,56 @@
+'use strict';
+
+module.exports = {
+  url: '',
+  pathPrefix: '/',
+  title: 'Blog by Madhan',
+  subtitle: `
+    Hi 👋 my name is Madhan (he/him). I\'/m a Frontend developer.
+    I have built web experiences for companies including Lenovo, Novopay,
+    Blackbuck, using a broad range of technologies,
+    most notably JavaScript, React, Redux, Angular, CSS.
+  `,
+  copyright: '',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: '',
+  useKatex: false,
+  menu: [
+    // {
+    //   label: 'Articles',
+    //   path: '/'
+    // },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    // {
+    //   label: 'Contact me',
+    //   path: '/pages/contacts'
+    // }
+  ],
+  author: {
+    name: 'Madhankumar',
+    photo: '/me.jpg',
+    bio: `
+      People oriented human. Frontend developer.
+    `,
+    contacts: {
+      email: '',
+      facebook: '',
+      telegram: '',
+      twitter: 'madddyrules',
+      github: 'madhankumar028',
+      rss: '',
+      vkontakte: '',
+      linkedin: 'madddymadhan',
+      instagram: '',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+    }
+  }
+};
