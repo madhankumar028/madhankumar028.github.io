8ed91ce626acaad6842fe8931a39ae158c6a990d
madhankumar028 <madhankumar028@gmail.com>
2020-05-04 08:36:12 +0700

    initial commit

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,25 @@
+module.exports = [{
+      plugin: require('../node_modules/gatsby-remark-images/gatsby-browser.js'),
+      options: {"plugins":[],"maxWidth":960,"withWebp":true,"ignoreFileExtensions":[]},
+    },{
+      plugin: require('../node_modules/gatsby-remark-autolink-headers/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-netlify-cms/gatsby-browser.js'),
+      options: {"plugins":[],"modulePath":"/Users/madhankumar/dev/pet/madhankumar028.github.io/src/cms/index.js"},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-google-gtag/gatsby-browser.js'),
+      options: {"plugins":[],"trackingIds":[""],"pluginConfig":{"head":true}},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
+      options: {"plugins":[],"name":"Blog by Madhan","short_name":"Blog by Madhan","start_url":"/","background_color":"#FFF","theme_color":"#F7A046","display":"standalone","icon":"static/photo.jpg"},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-offline/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../node_modules/gatsby-plugin-catch-links/gatsby-browser.js'),
+      options: {"plugins":[]},
+    },{
+      plugin: require('../gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ content/pages/about.md
@@ -0,0 +1,13 @@
+---
+title: "About me"
+template: "page"
+socialImage: "/media/about_me_cover.jpg"
+---
+
+I'm a Frontend developer. I have built web experiences for industries like Media, IoT, Payments. Logistics using a broad range of technologies, most notably JavaScript, Angular, React, Redux, CSS.
+
+![](/media/about_me_cover.jpg)
+
+With the web industry constantly evolving, I am passionate about curating new technologies and frameworks to make it easier for developers to decide what best fits their needs.
+
+On this blog I write about the lessons I learn in my daily life, mostly with JavaScript, React, Angular, CSS, Dev experience, and anything else front-end related.
