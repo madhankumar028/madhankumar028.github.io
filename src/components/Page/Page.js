ecf8ff9358c8f58bfb0826fe4e778c0af930ec4f
madhankumar028 <madhankumar028@gmail.com>
2020-05-07 19:29:36 +0700

    adds the conditional pagination renders

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import styles from './Page.module.scss';
 
 type Props = {
@@ -6,23 +6,15 @@ type Props = {
   children: React.Node
 };
 
-const Page = ({ title, children }: Props) => {
-  const pageRef = useRef();
-
-  useEffect(() => {
-    // pageRef.current.scrollIntoView();
-  });
-
-  return (
-    <div ref={pageRef} className={styles['page']}>
-      <div className={styles['page__inner']}>
-        { title && <h1 className={styles['page__title']}>{title}</h1>}
-        <div className={styles['page__body']}>
-          {children}
-        </div>
+const Page = ({ title, children }: Props) => (
+  <div ref={pageRef} className={styles['page']}>
+    <div className={styles['page__inner']}>
+      { title && <h1 className={styles['page__title']}>{title}</h1>}
+      <div className={styles['page__body']}>
+        {children}
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default Page;
