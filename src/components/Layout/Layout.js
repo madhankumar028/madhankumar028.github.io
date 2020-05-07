3942b961765ee0c9b87f7a3da1fe0101d6cef6ba
madhankumar028 <madhankumar028@gmail.com>
2020-05-08 00:15:21 +0700

    adds theme provider to the context

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
 // @flow strict
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Helmet from 'react-helmet';
 import { withPrefix } from 'gatsby';
 import type { Node as ReactNode } from 'react';
@@ -22,6 +22,38 @@ const Layout = ({
   const { author, url } = useSiteMetadata();
   const metaImage = socialImage != null ? socialImage : author.photo;
   const metaImageUrl = url + withPrefix(metaImage);
+  const [theme, setTheme] = useState('')
+
+  const setStyles = (element, params) => {
+    for(let i in params) {
+      element.style.setProperty(i, params[i]);
+    }
+  }
+  
+  // Main logic
+  const themeChange = (event) => {
+    let element;
+    if (event) {
+      element = event.target || event.currentTarget;
+    } else {
+      element = document.getElementById('toggle');
+    }
+    const body = document.querySelector("body");
+
+    setTheme(theme === 'light' ? 'dark' : 'light')
+
+    if (theme === "dark") {
+      setStyles(body, {"--background": "#FBFFF1", "--text": "#3C3744", "--accent": "#F49F6E"});
+      element.classList.remove(`${styles.clicked}`);
+    } else {
+      setStyles(body, {"--background": "#3C3744", "--text": "#FBFFF1", "--accent": "#C41E3D"});
+      element.classList.add(`${styles.clicked}`);
+    }
+  }
+
+  useEffect(() => {
+    themeChange();
+  }, [])
 
   return (
     <div className={styles.layout}>
@@ -34,7 +66,11 @@ const Layout = ({
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={metaImageUrl} />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css" rel="stylesheet" />
       </Helmet>
+      <div className={styles['theme__toggler']}>
+        <button className={styles['toggle']} id="toggle" onClick={(event) => themeChange(event)}></button>
+      </div>
       {children}
     </div>
   );
